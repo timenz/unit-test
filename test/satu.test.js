52b11261b2f6aa4a685fc68b0d44e1a153fb7084
banxtim <timen@mtarget.co>
2023-12-12 20:20:20 +0700

    create example unit test #2

--- test/satu.test.js
+++ test/satu.test.js
@@ -4,3 +4,11 @@ import { jumlah } from '../src/satu'
 test('1 tambah 2 sama dengan 3', () => {
   expect(jumlah(1, 2)).toBe(3)
 })
+
+test('0 tambah 2 sama dengan 2', () => {
+  expect(jumlah(0, 2)).toBe(2)
+})
+
+test('return null jika memasukkan parameter selain number', () => {
+  expect(jumlah('satu', 'dua')).toBe(null)
+})
